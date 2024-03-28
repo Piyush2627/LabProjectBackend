3cb8572fa2dc29782e4e414e9a90114190d8de7a
Piyush2627 <piyushdahiwadkar89@gmail.com>
2024-03-28 08:05:59 +0530

    data sent

--- routers/workout.js
+++ routers/workout.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Workout = require("../models/Workout");
 
 router.get("/", (req, res) => {
   res.json({ success: "Get app workout" });
@@ -9,7 +10,14 @@ router.get("/:id", (req, res) => {
   res.json({ success: "Get one workout" });
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
   res.json({ success: "Add one workout" });
 });
 
