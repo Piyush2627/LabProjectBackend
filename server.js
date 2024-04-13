5c8caa56731ce83f3b528643d16f29f54776ce0e
Piyush2627 <piysuhdahiwadkar89@gmail.com>
2024-04-13 09:08:04 +0530

    event

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const WorkoutsRouter = require("./routes/workouts");
 const StudentsRouter = require("./routes/Students");
 const AttedanceRouter = require("./routes/Attendance");
 const cors = require("cors");
@@ -12,7 +11,6 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/api/workouts", WorkoutsRouter);
 app.use("/api/students", StudentsRouter);
 app.use("/api/attendance", AttedanceRouter);
 
